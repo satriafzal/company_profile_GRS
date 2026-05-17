d254b8463226ca2691d1c61f86a855df382cbdd9
catiulll <satriafzal@gmail.com>
2026-05-17 15:16:15 +0700

    update main js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
+
+// initialize aos
+AOS.init({
+    duration : 1000.,
+    once: true,
+    offset: 100
+})
 
 app.use(router)
 
